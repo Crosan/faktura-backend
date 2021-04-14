3ee9bfb6f8e5f7c427a70320c25a4b7b823a4f55
Roald Simonsen <31620386+archStant@users.noreply.github.com>
2021-04-14 17:23:46 +0200

    Changes to db structure

---
+++ backend/faktura/extra/typechoices.py
@@ -0,0 +1,5 @@
+from model_utils import Choices
+
+TypeChoices = Choices(('Labka', 'Labka'),
+                      ('Blodbank', 'Blodbank'),
+                      ('Andet', 'Andet'))
--- backend/faktura/models/rekvirent.py
+++ backend/faktura/models/rekvirent.py
@@ -6,9 +6,13 @@ from django.utils.timezone import now
 class Rekvirent(models.Model):
     # hospital = models.CharField(max_length=100)
     niveau = models.CharField(max_length=50)
-    afdelingsnavn = models.CharField(max_length=100)
-    GLN_nummer = models.CharField(max_length=100)
+    shortname = models.CharField(max_length=100)
+    rekv_nr = models.CharField(max_length=255)
+    GLN_nummer = models.CharField(max_length=100, null=True)
     betalergruppe = models.ForeignKey(
         'Betalergruppe', related_name='rekvirenter', on_delete=models.PROTECT, blank=True, null=True)
+
+    def __str__(self):
+        return "%d - %s" % (self.id, self.shortname)
 
     # objects = models.Manager()
