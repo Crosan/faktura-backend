b65b32399f4fad4978bfbe1ef7476f4edbd1219c
Roald Simonsen <31620386+archStant@users.noreply.github.com>
2021-06-28 09:52:38 +0200

    USING

--- backend/faktura/models/betalergruppe.py
+++ backend/faktura/models/betalergruppe.py
@@ -10,6 +10,7 @@ class Betalergruppe(models.Model):
     navn = models.CharField(max_length=100)
     bgtype = models.CharField(max_length=100, null=True, blank=True, choices=TypeChoices)
     oprettet = models.DateTimeField(default=now)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return "%s (%s)" % (self.navn, self.bgtype)
