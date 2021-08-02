6a237261973dcf48c4d1a23c6e2614c7fb4fb891
Roald Simonsen <31620386+archStant@users.noreply.github.com>
2021-08-02 09:56:18 +0200

    Simpler solution to regions

--- backend/faktura/models/analyse_type.py
+++ backend/faktura/models/analyse_type.py
@@ -10,8 +10,13 @@ class AnalyseType(models.Model):
     gruppering = models.CharField(max_length=100, blank=True, null=True)
     afdeling = models.CharField(max_length=50, blank=True, null=True)
     type = models.CharField(max_length=100, blank=True, null=True)
-    kilde_navn = models.CharField(max_length=100, blank=True, null=True) 
-    ignore = models.ManyToManyField(Region)
+    kilde_navn = models.CharField(max_length=100, blank=True, null=True)
+    regionh = models.BooleanField(default=True) #Skal denne analysetype faktureres til RegionH?
+    sjaelland = models.BooleanField(default=True)
+    syddanmark = models.BooleanField(default=True)
+    nordjylland = models.BooleanField(default=True)
+    midtjylland = models.BooleanField(default=True)
+    # ignore = models.ManyToManyField(Region)
 
     objects = models.Manager()
 
