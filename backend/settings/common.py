936e83914cec1d15db8c2ef2a1f387554f1582aa
Roald Simonsen <31620386+archStant@users.noreply.github.com>
2021-08-26 10:55:08 +0200

    Add secret key to common settings

--- backend/settings/common.py
+++ backend/settings/common.py
@@ -16,6 +16,8 @@ import os
 
 logger = logging.getLogger(__name__)
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
